343d5eaf682c7f23301cec6162aba0651335f577
JBrunoBarbosa <jbruno356@gmail.com>
2023-11-26 13:34:51 -0300

    added: unit tests comments

--- test/unit/UnitModel.hpp
+++ test/unit/UnitModel.hpp
@@ -1,22 +1,98 @@
+/**
+ * @brief Arquivo de testes unitários para a classe Model.
+ * 
+ * Este arquivo contém declarações de métodos de teste para a classe Model.
+ */
 #ifndef UNIT_MODEL_HPP
 #define UNIT_MODEL_HPP
+
 #include <math.h>
 #include "UnitSystem.hpp"
 #include "UnitFlow.hpp"
 #include "../../src/ModelImpl.hpp"
-
+ 
+/**
+ * @brief Testa o construtor da classe Model.
+ * 
+ * Este método verifica se o construtor da classe Model funciona conforme esperado.
+ */
 void unit_Model_constructor();
-void unit_Model_beginSystems();
-void unit_Model_endSystems();
-void unit_Model_beginFlows();
-void unit_Model_endFlows();
-void unit_Model_beginHistory();
-void unit_Model_endHistory();
-void unit_Model_add();
-void unit_Model_setInitialHistory();
-void unit_Model_clearModel();
-void unit_Model_execute();
-void run_unit_tests_Model();
 
+/**
+ * @brief Testa o método beginSystems da classe Model.
+ * 
+ * Este método verifica se o método beginSystems inicia adequadamente os sistemas no modelo.
+ */
+void unit_Model_beginSystems();
+
+/**
+ * @brief Testa o método endSystems da classe Model.
+ * 
+ * Este método verifica se o método endSystems conclui adequadamente os sistemas no modelo.
+ */
+void unit_Model_endSystems();
+
+/**
+ * @brief Testa o método beginFlows da classe Model.
+ * 
+ * Este método verifica se o método beginFlows inicia adequadamente os fluxos no modelo.
+ */
+void unit_Model_beginFlows();
+
+/**
+ * @brief Testa o método endFlows da classe Model.
+ * 
+ * Este método verifica se o método endFlows conclui adequadamente os fluxos no modelo.
+ */
+void unit_Model_endFlows();
+
+/**
+ * @brief Testa o método beginHistory da classe Model.
+ * 
+ * Este método verifica se o método beginHistory inicia adequadamente o histórico no modelo.
+ */
+void unit_Model_beginHistory();
+
+/**
+ * @brief Testa o método endHistory da classe Model.
+ * 
+ * Este método verifica se o método endHistory conclui adequadamente o histórico no modelo.
+ */
+void unit_Model_endHistory();
+
+/**
+ * @brief Testa o método add da classe Model.
+ * 
+ * Este método verifica se o método add adiciona corretamente um elemento ao modelo.
+ */
+void unit_Model_add();
+
+/**
+ * @brief Testa o método setInitialHistory da classe Model.
+ * 
+ * Este método verifica se o método setInitialHistory atribui corretamente o histórico inicial ao modelo.
+ */
+void unit_Model_setInitialHistory();
+
+/**
+ * @brief Testa o método clearModel da classe Model.
+ * 
+ * Este método verifica se o método clearModel limpa corretamente o modelo.
+ */
+void unit_Model_clearModel();
+
+/**
+ * @brief Testa o método execute da classe Model.
+ * 
+ * Este método verifica se o método execute executa corretamente a lógica do modelo.
+ */
+void unit_Model_execute();
+
+/**
+ * @brief Função para executar todos os testes unitários para a classe Model.
+ * 
+ * Esta função chama todos os testes unitários definidos para a classe Model.
+ */
+void run_unit_tests_Model();
 
 #endif
--- test/unit/UnitSystem.hpp
+++ test/unit/UnitSystem.hpp
@@ -1,15 +1,68 @@
+/**
+ * @brief Arquivo de testes unitários para a classe System.
+ * 
+ * Este arquivo contém declarações de métodos de teste para a classe System.
+ */
 #ifndef UNIT_SYSTEM_HPP
 #define UNIT_SYSTEM_HPP
+
 #include <assert.h>
 #include "../../src/SystemImpl.hpp"
 
+/**
+ * @brief Testa o construtor da classe System.
+ * 
+ * Este método verifica se o construtor da classe System funciona conforme esperado.
+ */
 void unit_System_constructor();
+
+/**
+ * @brief Testa o método setName da classe System.
+ * 
+ * Este método verifica se o método setName atribui corretamente o nome ao sistema.
+ */
 void unit_System_setName();
+
+/**
+ * @brief Testa o método setValue da classe System.
+ * 
+ * Este método verifica se o método setValue atribui corretamente o valor ao sistema.
+ */
 void unit_System_setValue();
+
+/**
+ * @brief Testa o método getName da classe System.
+ * 
+ * Este método verifica se o método getName retorna corretamente o nome do sistema.
+ */
 void unit_System_getName();
+
+/**
+ * @brief Testa o método getValue da classe System.
+ * 
+ * Este método verifica se o método getValue retorna corretamente o valor do sistema.
+ */
 void unit_System_getValue();
+
+/**
+ * @brief Testa o operador de atribuição da classe System.
+ * 
+ * Este método verifica se o operador de atribuição da classe System funciona corretamente.
+ */
 void unit_System_operator();
+
+/**
+ * @brief Testa o método clone da classe System.
+ * 
+ * Este método verifica se o método clone cria corretamente uma cópia do sistema.
+ */
 void unit_System_clone();
+
+/**
+ * @brief Função para executar todos os testes unitários para a classe System.
+ * 
+ * Esta função chama todos os testes unitários definidos para a classe System.
+ */
 void run_unit_tests_System();
 
 #endif
